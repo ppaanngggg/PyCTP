d601300cbab4ba323a93087f94d6922138b30db7
ppaanngggg <hantian.pang@gmail.com>
2017-04-21 10:29:16 +0800

    use 3.6

--- install_python3.sh
+++ install_python3.sh
@@ -3,5 +3,5 @@ sudo cp ./api/thostmduserapi.so /usr/lib/libthostmduserapi.so
 sudo cp ./api/thosttraderapi.so /usr/lib/libthosttraderapi.so
 
 swig -c++ -python -py3 -threads PyCTP.i
-python3 setup.py build_ext
-sudo python3 setup.py install
+python3.6 setup.py build_ext
+sudo python3.6 setup.py install
